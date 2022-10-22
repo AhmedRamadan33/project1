b471d9baff1edce3218949e78c8366e0128445ec
unknown <Ahmed-Ramadan-Abdelaaty>
2022-10-22 15:08:38 +0200

    create project

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+
+
+window.onscroll = function(){
+    if(scrollY >= 400){
+      $(".fixed_icon").css("visibility" , "visible")  
+    }
+    else{
+      $(".fixed_icon").css("visibility" , "hidden")  
+      
+    }
+    
+  }
+
+
+
+
+
+
+  
+// let fixed_icon = document.querySelector(".fixed_icon")
+
+// window.onscroll = function(){
+//   if(scrollY >= 400){
+//     fixed_icon.style.visibility = "visible"
+//   }
+//   else{
+//     fixed_icon.style.visibility = "hidden"
+
+//   }
+  
+// }
